8cbc2e37b9d84e1bbeda55e4460188514918c6c1
EGalinevsky <e.galinevsky@gmail.com>
2021-10-29 07:41:12 +0000

    add new BTN

--- src/components/useReducer/useReducer.jsx
+++ src/components/useReducer/useReducer.jsx
@@ -6,14 +6,40 @@ import { AlertProvider } from "./alert/alertContainer";
 
 
 export const Hooks7 = () => {
-
-
+  const[show, setShow] = useState(false)
+  const arr = [
+    {
+      name: 'gluk',
+      age: 20,
+      id: 1
+    },
+    {
+      name: 'Lexa',
+      age: 20,
+      id: 2
+    },
+    {
+      name: 'Karl',
+      age: 20,
+      id: 3
+    },
+    {
+      name: 'Daril',
+      age: 20,
+      id: 4
+    },
+    {
+      name: 'Marlen',
+      age: 20,
+      id: 5
+    }
+  ]
   return (
     <AlertProvider>
       <div>
         <div>
           useContext
-          <ContextContainer  toggleAlert={()=>{}} />
+          <ContextContainer array={arr} showBtns={show} setShowBtns={setShow}/>
           <Alert />
         </div>
       </div>
--- src/components/useReducer/useReducerContainer.jsx
+++ src/components/useReducer/useReducerContainer.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { useAlert, useToggleAlert } from "./alert/alertContainer";
 
-export default function     ContextContainer() {
-
+export default function     ContextContainer({array, showBtns, setShowBtns}) {
   const {show,visible, hide} = useAlert()
+
   const style ={
     color: 'blue',
     font: 'bold',
   }
+  
+  const handleClickBTN = () =>{
+    setShowBtns(prev => !prev )
+  }
+
   return (
     <>
       <h1 style={style}>Hello in example context </h1>
-      <button onClick={!visible ? show: hide}>{!visible ? 'show': 'hide'} alert</button>      
+      <button onClick={handleClickBTN}>
+        show Button
+      </button>
+      {showBtns && 
+      <div>
+        {array.map(a => <button key={a.id}> 
+          {a.name}
+        </button>)}
+      </div>}
+      <div>
+        <button onClick={!visible ? show: hide}>{!visible ? 'show': 'hide'} alert</button>    
+      </div>
+      
     </>
   );
 }
