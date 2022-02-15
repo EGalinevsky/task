1dd411ffddfb3cf8c7a47ee3d8a57c3fd61ee734
Egalinevksy <eugene.galinevsky@mail.ru>
2022-02-15 10:19:00 +0300

    some

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css';
+import { Hooks1 } from './components/useState/useStateHooks';
+import { Hooks2 } from './components/useEffect/useEffectHooks';
+import { Hooks3 } from './components/useRef/useRefHooks';
+import { Hooks4 } from './components/useMemo/useMemo';
+import { Hooks5 } from './components/useCallback/useCallback';
+import { Hooks6 } from './components/useContext/useContext';
+import { Hooks7 } from './components/useReducer/useReducer';
+
+function App() {
+  return (
+    <div className="App">      
+      {/* <Hooks1 /> */}
+      {/* <Hooks2 /> */}
+      {/* <Hooks3 /> */}
+      {/* <Hooks4 /> */}
+      {/* <Hooks5 /> */}
+      {/* <Hooks6 /> */}
+      {/* <Hooks7/> */}
+      learn react
+    </div>
+  );
+}
+
+export default App;
